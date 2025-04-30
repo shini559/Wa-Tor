5becec77f71e928e3ccfd9cefe7d78a4570a7063
shooter-dev <vincentbleach@gmail.com>
2025-04-30 10:36:42 +0200

    feat: create function move in fish

--- src/core/fish.py
+++ src/core/fish.py
@@ -15,5 +15,15 @@ class Fish:
         self.x: int = x
         self.y: int = y
 
+        self.last_x: int = x
+        self.last_y: int = y
+
         self.age = 0
         self.reproduction_timer: int = TUNA_REPRODUCTION_TIME
+
+    def move(self, x: int, y: int):
+        self.last_x = self.x
+        self.last_y = self.y
+
+        self.x = x
+        self.y = y
